eb7db0a0ea9b1797916adf306062c00dff4c5d79
JeanThirion <jean.thirion@esial.net>
2021-03-19 10:18:53 +1100

    demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebootcamp-crm-march2021';
+  title = 'Firebootcamp CRM';
+  someDate = new Date();
+
+
+
 }
