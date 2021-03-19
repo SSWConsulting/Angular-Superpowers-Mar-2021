4b467cd71c9edbec8af9fbb14564d6f5c71c34ef
JeanThirion <jean.thirion@esial.net>
2021-03-19 14:28:07 +1100

    added routing Added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
+import { CompanyListComponent } from './company/company-list/company-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'company/list', pathMatch: 'full'},
+  { path: 'company/list', component: CompanyListComponent },
+  { path: 'company/new', component: CompanyEditComponent },
+  { path: 'company/edit/:id', component: CompanyEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
